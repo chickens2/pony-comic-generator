9af7b589e9c4b66eccc0ddbce5a7649f0af97ede
The Public Clop Accountant <PCAF@maresmating.club>
2017-01-11 01:59:31 -0500

    Improve regex

--- backgrounds/Resize.sh
+++ backgrounds/Resize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find -E . -regex ".*\.(jpg|gif|png|jpeg)" -print0 | xargs -0 mogrify -resize "500^>"
+find -E . -iregex ".*\.(jpg|gif|png|jpeg)" -print0 | xargs -0 mogrify -resize "500^>"
