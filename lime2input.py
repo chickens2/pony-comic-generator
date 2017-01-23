36f7b1a7eb54e4b8e78abb3877028afa7128789c
The Public Clop Accountant <PCAF@maresmating.club>
2017-01-23 00:54:21 -0500

    MVP for lime2input
    Currently works just fine for copied text.
    Don't try it with a file right now, unless you use the '-A 0' command flag

--- lime2input.py
+++ lime2input.py
@@ -3,3 +3,86 @@
 # -*- coding: UTF-8 -*-
 # vim: set fileencoding=UTF-8 :
 
+import pyperclip
+import StringIO
+import sys
+import os
+import utilFunctions
+import string
+
+"""
+Converts LimeChat-formatted logs into the expected format for the comic parses
+Makes intelligent guesses at what should become a /me statement, since those aren't stored by LimeChat
+If input/output filenames aren't specified, the clipboard will suffice for the missing filename
+Expected format is "hh:mm username: message" for input files
+
+guessMode (set with -A command) options:
+0: only turn lines without a colon after the username into a /me command (useful for copying from clipboard)
+1: run all lines through the /me guesser
+2: ignore the case sensitivity of the /me guesser
+"""
+
+# command line options
+nextToFill= None # iterator
+limeLog = None # input log
+outputLog = None # converted log
+guessMode = 0
+for arg in sys.argv:
+	if arg[0] == '-':
+		nextToFill = arg[1:].lower()
+	else:
+		if nextToFill is not None:
+			if nextToFill[0] == 'i':
+				limeLog = arg
+			if nextToFill[0] == 'o':
+				outputLog = arg
+			if nextToFill[0] == 'A':
+				guessMode = int(arg)
+
+#
+def convertLimelog(limechat,gm):
+	newchat = []
+	rawwords = []
+	for line in limechat:
+		rawwords.append(line.split())
+		# rawwords should be an array of arrays, with the inner array being ["hh:mm", "nick", "word1", "word2", etc…]
+	if gm > 0:
+		aids=8
+	else:
+		for wordlist in rawwords:
+			newline = ""
+			if ':' in wordlist[1]: # normal line
+				newline = "<" + wordlist[1][:-1] + ">" # <nick>
+			else:
+				newline = "* " + wordlist[1] # * nick
+			for word in wordlist[2:]:
+				newline += " "
+				newline += word
+			newline += '\n'
+			newchat.append(newline)
+
+
+	return newchat
+
+# assumes that any usernames that are the first
+def processLine(name,message,guessstrat):
+	return words
+
+chatfile = None
+if limeLog is None:
+	clipboard = pyperclip.paste().encode('utf8')
+	chatfile = StringIO.StringIO(clipboard)
+else:
+	if '-A' not in sys.argv:
+		guessMode == 1
+	chatfile = open(limeLog).readlines()
+
+
+newlog = convertLimelog(chatfile, guessMode)
+
+if outputLog is None:
+	pyperclip.copy("".join(newlog))
+else:
+	file = open(outputLog, 'w')
+	file.writelines(newlog)
+	file.close()
